ce23d82f5dbb76a827469590070fbc96c46a803a
Chinseok Lee <me@pyhub.kr>
2023-11-21 17:26:20 +0900

    formatted by black

--- mydjango01/mysite/urls.py
+++ mydjango01/mysite/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+
 def root(request):
     return HttpResponse("hello vscode")
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', root),
+    path("admin/", admin.site.urls),
+    path("", root),
 ]
