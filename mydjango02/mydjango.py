87860d04f9b946cc2358e90990ce4067fa8e02da
Chinseok Lee <me@pyhub.kr>
2023-11-22 10:35:26 +0900

    외부 데이터로 웹문서 응답하기

--- mydjango02/mydjango.py
+++ mydjango02/mydjango.py
@@ -25,13 +25,17 @@ django.setup()
 
 
 def index(request):
-    query = "악뮤"  # 검색어
-
     json_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230906.json"
-    response = requests.get(json_url)
-    response.raise_for_status()
-    song_list = response.json()
 
+    response = requests.get(json_url)
+    # response.raise_for_status()
+    if response.ok:
+        song_list = response.json()
+    else:
+        song_list = []
+
+    # query = "Love"  # 검색어
+    #
     # song_list = [
     #     {"곡명": "Seven (feat. Latto) - Clean Ver.", "가수": "정국"},
     #     {"곡명": "Love Lee", "가수": "AKMU (악뮤)"},
@@ -39,7 +43,7 @@ def index(request):
     #     {"곡명": "후라이의 꿈", "가수": "AKMU (악뮤)"},
     #     {"곡명": "어떻게 이별까지 사랑하겠어, 널 사랑하는 거지", "가수": "AKMU (악뮤)"},
     # ]
-    # 파이썬 빌트인 함수 filter를 활용해서, 곡명에 검색어가 포함된 노래만 필터링
+    # # 파이썬 빌트인 함수 filter를 활용해서, 곡명에 검색어가 포함된 노래만 필터링
     # song_list = filter(lambda song: query in song["가수"] or query in song["곡명"], song_list)
 
     return render(request, "index.html", {"song_list": song_list})
