07019bca3377aaa0c73655f670ab63325b318f28
Chinseok Lee <me@pyhub.kr>
2023-11-23 13:28:21 +0900

    [mydjango03-hottrack] hottrack 시작 페이지 템플릿

--- mydjango03-hottrack/hottrack/views.py
+++ mydjango03-hottrack/hottrack/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="hottrack/index.html", context={})
