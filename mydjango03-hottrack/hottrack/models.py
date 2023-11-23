db33c532280a06d8cf6380826cd3aef72dec3c41
Chinseok Lee <me@pyhub.kr>
2023-11-23 13:54:07 +0900

    [mydjango03-hottrack] 멜론 TOP100 데이터 렌더링

--- mydjango03-hottrack/hottrack/models.py
+++ mydjango03-hottrack/hottrack/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+# hottrack/models.py
 
-# Create your models here.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import date
+from typing import Dict
+from urllib.parse import quote
+
+
+# 파이썬 3.7부터 지원
+@dataclass
+class Song:
+    id: str
+    rank: int
+    album_name: str
+    name: str
+    artist_name: str
+    cover_url: str
+    lyrics: str
+    genre: str
+    release_date: date
+    like_count: int
+
+    @property
+    def melon_detail_url(self) -> str:
+        melon_uid = quote(self.id)
+        return f"https://www.melon.com/song/detail.htm?songId={melon_uid}"
+
+    @property
+    def youtube_search_url(self) -> str:
+        search_query = quote(f"{self.name}, {self.artist_name}")
+        return f"https://www.youtube.com/results?search_query={search_query}"
+
+    @classmethod
+    def from_dict(cls, data: Dict) -> Song:
+        return cls(
+            id=data.get("곡일련번호"),
+            rank=int(data.get("순위")),
+            album_name=data.get("앨범"),
+            name=data.get("곡명"),
+            artist_name=data.get("가수"),
+            cover_url=data.get("커버이미지_주소"),
+            lyrics=data.get("가사"),
+            genre=data.get("장르"),
+            release_date=date.fromisoformat(data.get("발매일")),
+            like_count=int(data.get("좋아요")),
+        )
--- mydjango03-hottrack/hottrack/views.py
+++ mydjango03-hottrack/hottrack/views.py
@@ -1,6 +1,22 @@
+import json
+from urllib.request import urlopen
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from hottrack.models import Song
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name="hottrack/index.html", context={})
+    melon_chart_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230910.json"
+    json_string = urlopen(melon_chart_url).read().decode("utf-8")
+    # 외부 필드명을 그대로 쓰기보다, 내부적으로 사용하는 필드명으로 변경하고, 필요한 메서드를 추가합니다.
+    song_list = [Song.from_dict(song_dict) for song_dict in json.loads(json_string)]
+
+    return render(
+        request=request,
+        template_name="hottrack/index.html",
+        context={
+            "song_list": song_list,
+        },
+    )
