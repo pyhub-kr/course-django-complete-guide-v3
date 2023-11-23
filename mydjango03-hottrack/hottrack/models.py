af6c3d8462b746eef4dababeae960943352dc9ed
Chinseok Lee <me@pyhub.kr>
2023-11-23 14:52:43 +0900

    [mydjango03-hottrack] 장고 모델과 데이터베이스 활용

--- mydjango03-hottrack/hottrack/models.py
+++ mydjango03-hottrack/hottrack/models.py
@@ -2,29 +2,28 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
 from datetime import date
 from typing import Dict
 from urllib.parse import quote
 
+from django.db import models
 
-# 파이썬 3.7부터 지원
-@dataclass
-class Song:
-    id: str
-    rank: int
-    album_name: str
-    name: str
-    artist_name: str
-    cover_url: str
-    lyrics: str
-    genre: str
-    release_date: date
-    like_count: int
+
+class Song(models.Model):
+    melon_uid = models.CharField(max_length=20, unique=True)
+    rank = models.PositiveSmallIntegerField()
+    album_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    cover_url = models.URLField()
+    lyrics = models.TextField()
+    genre = models.CharField(max_length=100)
+    release_date = models.DateField()
+    like_count = models.PositiveIntegerField()
 
     @property
     def melon_detail_url(self) -> str:
-        melon_uid = quote(self.id)
+        melon_uid = quote(self.melon_uid)
         return f"https://www.melon.com/song/detail.htm?songId={melon_uid}"
 
     @property
@@ -35,7 +34,7 @@ class Song:
     @classmethod
     def from_dict(cls, data: Dict) -> Song:
         return cls(
-            id=data.get("곡일련번호"),
+            melon_uid=data.get("곡일련번호"),
             rank=int(data.get("순위")),
             album_name=data.get("앨범"),
             name=data.get("곡명"),
