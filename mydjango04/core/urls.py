24e3bff8803bc44c01a88383c3c30edd7e5c4f5e
Chinseok Lee <me@pyhub.kr>
2023-11-24 20:07:39 +0900

    [메시지 프레임워크] 기본 예시 추가

--- mydjango04/core/urls.py
+++ mydjango04/core/urls.py
@@ -3,4 +3,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index),
+]
--- mydjango04/core/views.py
+++ mydjango04/core/views.py
@@ -1,3 +1,12 @@
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    messages.debug(request, "디버그 메시지")
+    messages.info(request, "정보 메시지")
+    messages.success(request, "성공 메시지")
+    messages.warning(request, "경고 메시지")
+    messages.error(request, "에러 메시지")
+
+    return render(request, "core/index.html")
