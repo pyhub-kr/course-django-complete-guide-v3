d18c2147be5ca82b6634981a028498ef5d83743d
Chinseok Lee <me@pyhub.kr>
2023-11-30 16:24:49 +0900

    [장고 기본 CBV API (Base Views)] 최상위 주소에 대한 3가지 처리 방법 예시

--- mydjango04/core/urls.py
+++ mydjango04/core/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from . import views
 
+app_name = "core"  # URL Reverse를 위한 Namespace 지정
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),  # URL Reverse를 위한 URL Pattern명 지정
 ]
--- mydjango04/mysite/settings.py
+++ mydjango04/mysite/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "mysite" / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
