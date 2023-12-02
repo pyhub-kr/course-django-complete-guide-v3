e0884671c5f0093d4e25e9679fbf37bf17bd4f01
Chinseok Lee <me@pyhub.kr>
2023-12-02 17:35:43 +0900

    [장고 기본 CBV API (Generic display views)] index 함수 기반 뷰의 ListView 버전

--- mydjango04/hottrack/views.py
+++ mydjango04/hottrack/views.py
@@ -9,47 +9,59 @@ import pandas as pd
 from django.db.models import QuerySet, Q
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from hottrack.models import Song
 from hottrack.utils.cover import make_cover_image
 
 
-def index(request: HttpRequest, release_date: datetime.date = None) -> HttpResponse:
-    query = request.GET.get("query", "").strip()
+class IndexView(ListView):
+    model = Song
+    template_name = "hottrack/index.html"
 
-    song_qs: QuerySet[Song] = Song.objects.all()
+    def get_queryset(self):
+        qs = super().get_queryset()
 
-    if release_date:
-        song_qs = song_qs.filter(release_date=release_date)
+        release_date = self.kwargs.get("release_date")
+        if release_date:
+            qs = qs.filter(release_date=release_date)
 
-    # melon_chart_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230910.json"
-    # json_string = urlopen(melon_chart_url).read().decode("utf-8")
-    # # 외부 필드명을 그대로 쓰기보다, 내부적으로 사용하는 필드명으로 변경하고, 필요한 메서드를 추가합니다.
-    # song_list = [Song.from_dict(song_dict) for song_dict in json.loads(json_string)]
+        query = self.request.GET.get("query", "").strip()
+        if query:
+            qs = qs.filter(
+                Q(name__icontains=query)
+                | Q(artist_name__icontains=query)
+                | Q(album_name__icontains=query)
+            )
 
-    if query:
-        song_qs = song_qs.filter(
-            Q(name__icontains=query)
-            | Q(artist_name__icontains=query)
-            | Q(album_name__icontains=query)
-        )
-        # song_list = [
-        #     song
-        #     for song in song_list
-        #     if query in song.name
-        #     or query in song.artist_name
-        #     or query in song.album_name
-        # ]
+        return qs
 
-    return render(
-        request=request,
-        template_name="hottrack/index.html",
-        context={
-            "song_list": song_qs,
-            "query": query,
-        },
-    )
+
+index = IndexView.as_view()
+
+# def index(request: HttpRequest, release_date: datetime.date = None) -> HttpResponse:
+#     query = request.GET.get("query", "").strip()
+#
+#     song_qs: QuerySet[Song] = Song.objects.all()
+#
+#     if release_date:
+#         song_qs = song_qs.filter(release_date=release_date)
+#
+#     if query:
+#         song_qs = song_qs.filter(
+#             Q(name__icontains=query)
+#             | Q(artist_name__icontains=query)
+#             | Q(album_name__icontains=query)
+#         )
+#
+#     return render(
+#         request=request,
+#         template_name="hottrack/index.html",
+#         context={
+#             "song_list": song_qs,
+#             "query": query,
+#         },
+#     )
 
 
 class SongDetailView(DetailView):
