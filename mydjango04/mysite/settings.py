b2b4d6fe64565d0f99a7268ccf9e44b514eb3605
Chinseok Lee <me@pyhub.kr>
2023-12-08 00:55:44 +0900

    postgresql 서버 접속 설정

--- mydjango04/mysite/settings.py
+++ mydjango04/mysite/settings.py
@@ -98,21 +98,31 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # }
 
 # pymysql의 경우 아래 코드를 통한 패치가 필요
-import pymysql  # noqa
-
-pymysql.install_as_MySQLdb()
+# import pymysql  # noqa
+#
+# pymysql.install_as_MySQLdb()
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "HOST": "localhost",  # 서버 주소
+#         "PORT": "3306",  # 서버 포트
+#         "NAME": "mysql_db",  # 데이터베이스 명
+#         "USER": "mysql_user",  # 유저명
+#         "PASSWORD": "mysql_pw",  # 암호
+#     }
+# }
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "HOST": "localhost",  # 서버 주소
-        "PORT": "3306",  # 서버 포트
-        "NAME": "mysql_db",  # 데이터베이스 명
-        "USER": "mysql_user",  # 유저명
-        "PASSWORD": "mysql_pw",  # 암호
+        "PORT": "5432",  # 서버 포트
+        "NAME": "mydb",  # 데이터베이스 명
+        "USER": "myuser",  # 유저명
+        "PASSWORD": "mypw",  # 암호
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
