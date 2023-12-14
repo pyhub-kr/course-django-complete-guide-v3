95df02f10f908af08dc404de1b980e15762eae99
Chinseok Lee <me@pyhub.kr>
2023-12-14 11:13:16 +0900

    [커스텀 모델 매니저] #2. models.QuerySet 상속

--- mydjango04/blog/models.py
+++ mydjango04/blog/models.py
@@ -3,15 +3,33 @@
 from django.db import models
 
 
-class PublishedPostManager(models.Manager):
-    def get_queryset(self):
-        qs = super().get_queryset()
-        qs = qs.filter(status=Post.Status.PUBLISHED)
-        return qs
+class PostQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(status=Post.Status.PUBLISHED)
+
+    def draft(self):
+        return self.filter(status=Post.Status.DRAFT)
+
+    def search(self, query: str):
+        return self.filter(title__contains=query)
+
+    # def by_author(self, author):
+    #     return self.filter(author=author)
 
     def create(self, **kwargs):
         kwargs.setdefault("status", Post.Status.PUBLISHED)
         return super().create(**kwargs)
+
+
+# class PublishedPostManager(models.Manager):
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         qs = qs.filter(status=Post.Status.PUBLISHED)
+#         return qs
+#
+#     def create(self, **kwargs):
+#         kwargs.setdefault("status", Post.Status.PUBLISHED)
+#         return super().create(**kwargs)
 
 
 class Post(models.Model):
@@ -34,8 +52,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)  # 최초 생성시각을 자동 저장
     updated_at = models.DateTimeField(auto_now=True)  # 매 수정시각을 자동 저장
 
-    published = PublishedPostManager()
-    objects = models.Manager()
+    # published = PublishedPostManager()
+    # objects = models.Manager()
+
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         # choices 속성을 사용한 필드는 get_필드명_display() 함수를 통해 레이블 조회를 지원합니다.
