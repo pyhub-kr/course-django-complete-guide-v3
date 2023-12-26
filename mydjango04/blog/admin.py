5e5688c41ca2a55e925cbbdbec06d576ab7525d2
Chinseok Lee <me@pyhub.kr>
2023-12-26 17:46:38 +0900

    [모델 Meta 필수 설정] admin 앱에서의 모델 표시 이름

--- mydjango04/blog/admin.py
+++ mydjango04/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
--- mydjango04/blog/models.py
+++ mydjango04/blog/models.py
@@ -92,6 +92,8 @@ class Post(models.Model):
         constraints = [
             UniqueConstraint(fields=["slug"], name="unique_slug"),
         ]
+        verbose_name = "포스팅"
+        verbose_name_plural = "포스팅 목록"
 
 
 class AccessLog(models.Model):
