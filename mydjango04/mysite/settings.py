470dd289e0b98dbbfab6003fd3d5161568265d38
Chinseok Lee <me@pyhub.kr>
2024-01-04 13:03:47 +0900

    디폴트 언어 설정을 ko-kr로 변경

--- mydjango04/mysite/settings.py
+++ mydjango04/mysite/settings.py
@@ -153,7 +153,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
 TIME_ZONE = "UTC"
 
