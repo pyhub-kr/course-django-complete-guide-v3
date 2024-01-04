6f23329d3798844d840b85e67fdf374b21881f2e
Chinseok Lee <me@pyhub.kr>
2024-01-04 16:52:25 +0900

    [모델 Meta 필수 설정] 데이터베이스 View를 장고 모델을 통한 조회

--- mydjango04/shop/models.py
+++ mydjango04/shop/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class ZipCode(models.Model):
     code = models.CharField(max_length=6)
     name = models.CharField(max_length=100)
+
+
+class JuniorEmployee(models.Model):
+    id = models.IntegerField(primary_key=True, db_column="employee_id")
+    first_name = models.CharField(max_length=50, db_column="employee_first_name")
+    last_name = models.CharField(max_length=50, db_column="employee_last_name")
+
+    class Meta:
+        managed = False
+        db_table = "junior_employee_view"
