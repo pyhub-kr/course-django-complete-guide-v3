03af8fade35cb8503cfd642463d004cb641478b6
Chinseok Lee <me@pyhub.kr>
2024-01-17 01:34:15 +0900

    admin의 inline 지원과 GenericForeignKey

--- mydjango04/weblog/admin.py
+++ mydjango04/weblog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline, GenericStackedInline
 
-# Register your models here.
+from .models import Post, Comment
+
+
+class CommentTabularInline(GenericTabularInline):
+    model = Comment
+    fields = ["message", "rating"]
+    extra = 0
+
+    # def has_change_permission(self, request, obj=None):
+    #     return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    # def has_add_permission(self, request, obj):
+    #     return False
+
+
+class CommentStackedInline(GenericStackedInline):
+    model = Comment
+    fields = ["message", "rating"]
+    extra = 0
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentTabularInline]
+    # inlines = [CommentStackedInline]
--- mydjango04/weblog/models.py
+++ mydjango04/weblog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -21,3 +22,9 @@ class Comment(models.Model):
     # on_delete 옵션을 지원하지 않습니다. CASCADE 정책으로만 동작합니다.
     content_object = GenericForeignKey(ct_field="content_type", fk_field="object_id")
     message = models.TextField()
+    rating = models.SmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ],
+    )
