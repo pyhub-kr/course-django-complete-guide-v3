de8208e10ea61049cf88f8ff311949799a7eef88
Chinseok Lee <me@pyhub.kr>
2024-01-29 01:12:58 +0900

    [weblog] commit=False 예시 (ip 필수 필드)

--- mydjango04/weblog/models.py
+++ mydjango04/weblog/models.py
@@ -19,6 +19,8 @@ class Post(models.Model):
     photo = models.ImageField(blank=True)
     created_date = models.DateField(auto_now_add=True)
 
+    ip = models.GenericIPAddressField()
+
     # Generic Relation에서는 1측에 관계를 정의하므로, 모델 클래스에 직접적으로 필드를 정의
     # 이 필드명으로 Comment에 대한 related_name, related_query_name 역할을 같이 수행
     comment_set = GenericRelation(to="Comment", related_query_name="post")
--- mydjango04/weblog/views.py
+++ mydjango04/weblog/views.py
@@ -13,7 +13,9 @@ def post_new(request):
         if form.is_valid():
             print("form.cleaned_data :", form.cleaned_data)
 
-            post = form.save(commit=True)
+            post = form.save(commit=False)
+            post.ip = request.META["REMOTE_ADDR"]
+            post.save()
 
             #
             # post = Post()
