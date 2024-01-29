5207022157a79cb8191d911baa58af8c5057b44c
Chinseok Lee <me@pyhub.kr>
2024-01-30 01:27:45 +0900

    [weblog] FormView를 활용한 포스팅 수정 뷰 구현 (ModelForm 버전)

--- mydjango04/weblog/views.py
+++ mydjango04/weblog/views.py
@@ -1,6 +1,10 @@
 from django.core.files.uploadedfile import UploadedFile
+from django.forms import model_to_dict
 from django.shortcuts import render, redirect, get_object_or_404
+
 from vanilla import FormView
+
+# from django.views.generic import FormView
 
 from weblog.forms import PostForm
 from weblog.models import Post
@@ -22,22 +26,30 @@ class PostCreateView(FormView):
 post_new = PostCreateView.as_view()
 
 
-def post_edit(request, pk):
-    instance = get_object_or_404(Post, pk=pk)
+class PostUpdateView(FormView):
+    form_class = PostForm
+    template_name = "weblog/post_form.html"
+    success_url = "/"
 
-    if request.method == "GET":
-        form = PostForm(instance=instance)
-    else:
-        form = PostForm(data=request.POST, files=request.FILES, instance=instance)
-        if form.is_valid():
-            post = form.save(commit=True)
-            # TODO: detail view 로 이동
-            return redirect("/")
+    # 장고 기본의 FormView 버전
+    # def get_form_kwargs(self):
+    #     post_pk = self.kwargs["pk"]
+    #     instance = get_object_or_404(Post, pk=post_pk)
+    #
+    #     form_kwargs = super().get_form_kwargs()
+    #     form_kwargs["instance"] = instance
+    #     return form_kwargs
 
-    return render(
-        request,
-        "weblog/post_form.html",
-        {
-            "form": form,
-        },
-    )
+    # django-vanilla-views의 FormView 버전
+    def get_form(self, data=None, files=None, **kwargs):
+        post_pk = self.kwargs["pk"]
+        instance = get_object_or_404(Post, pk=post_pk)
+        kwargs["instance"] = instance
+        return super().get_form(data=data, files=files, **kwargs)
+
+    def form_valid(self, form):
+        form.save(commit=True)
+        return super().form_valid(form)
+
+
+post_edit = PostUpdateView.as_view()
