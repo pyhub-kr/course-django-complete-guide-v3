4c7a2f1c3ba19499f63f371647875fb86a0cda6b
Chinseok Lee <me@pyhub.kr>
2024-01-31 16:53:42 +0900

    폼 유효성 검사 에러, 간단 CSS 스타일링

--- mydjango04/accounts/forms.py
+++ mydjango04/accounts/forms.py
@@ -3,6 +3,10 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["address"].required = True
+
     class Meta:
         model = Profile
         fields = ["address", "photo"]
