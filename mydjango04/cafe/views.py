0fcdcc1ac43133f4eecd2de954a2b460102ca1d9
Chinseok Lee <me@pyhub.kr>
2024-03-01 01:37:34 +0900

    SESSIONS 전역변수에 다양한 상탯값 담기

--- mydjango04/cafe/views.py
+++ mydjango04/cafe/views.py
@@ -1,14 +1,40 @@
+import random
+import string
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 
-ORDER_COUNTS = {}
+SESSIONS = {}
+
+
+def session_get_or_create(request):
+    global SESSIONS
+
+    session_id = request.COOKIES.get("session_id", "")
+    if session_id == "":
+        sample = string.ascii_lowercase + string.digits
+        while True:
+            session_id = "".join(random.choice(sample) for __ in range(32))
+            if session_id not in SESSIONS:
+                break
+        created = True
+    else:
+        created = False
+
+    return session_id, created
 
 
 @csrf_exempt
 def coffee_stamp(request):
-    global ORDER_COUNTS
+    global SESSIONS
+
+    # SESSIONS 전역변수에 session_id가 등록되어있지 않다면, 생성
+    session_id, session_created = session_get_or_create(request)
+    if session_id not in SESSIONS:
+        SESSIONS[session_id] = {"phone": "", "order_count": 0}
+    session = SESSIONS[session_id]
 
     if request.method == "GET":
         response = HttpResponse(
@@ -20,9 +46,9 @@ def coffee_stamp(request):
         )
     else:
         phone = request.POST["phone"]
-        order_count = ORDER_COUNTS.get(phone, 0)
+        order_count = session.get(phone, 0)
         order_count += 1
-        ORDER_COUNTS[phone] = order_count
+        session[phone] = order_count
 
         response = HttpResponse(
             f"""
@@ -31,20 +57,35 @@ def coffee_stamp(request):
             <a href="/cafe/free-coffee/">무료커피를 신청해주세요.</a>
             """
         )
-        response.set_cookie("phone", phone)
+
+    if session_created:
+        response.set_cookie("session_id", session_id)
 
     return response
 
 
 def coffee_free(request):
-    phone = request.COOKIES.get("phone", "")
+    global SESSIONS
+
+    # SESSIONS 전역변수에 session_id가 등록되어있지 않다면, 생성
+    session_id, session_created = session_get_or_create(request)
+    if session_id not in SESSIONS:
+        SESSIONS[session_id] = {"phone": "", "order_count": 0}
+    session = SESSIONS[session_id]
+
+    phone = session.get("phone", "")
     if not phone:
         return redirect("cafe:coffee_stamp")
 
-    order_count = ORDER_COUNTS.get(phone, 0)
+    order_count = session.get("order_count", 0)
     if order_count < 10:
-        return HttpResponse(
+        response = HttpResponse(
             f"{phone}님. 스탬프 {order_count}번 찍으셨어요. {10-order_count}번 찍으시면 무료쿠폰을 받을 수 있습니다."
         )
     else:
-        return HttpResponse(f"{phone}님. 무료쿠폰을 사용하시겠어요?")
+        response = HttpResponse(f"{phone}님. 무료쿠폰을 사용하시겠어요?")
+
+    if session_created:
+        response.set_cookie("session_id", session_id)
+
+    return response
