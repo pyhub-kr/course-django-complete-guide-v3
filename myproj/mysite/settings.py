63dba35285e60e3c64671366b7169e47145504c1
Chinseok Lee <me@pyhub.kr>
2024-03-25 02:59:21 +0900

    커스텀 유저 모델 등록

--- myproj/mysite/settings.py
+++ myproj/mysite/settings.py
@@ -120,6 +120,10 @@ DATABASES = {
 }
 
 
+# Custom User Model
+AUTH_USER_MODEL = "accounts.User"
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
