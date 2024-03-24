9bcc1192cbaa5280612abf730d91a52687bd91d0
Chinseok Lee <me@pyhub.kr>
2024-03-25 02:54:37 +0900

    modal-form 컴포넌트 추가 및 root.html 템플릿에 샘플 적용

---
+++ myproj/core/src-django-components/modal_form/modal_form.py
@@ -0,0 +1,12 @@
+from django_components.component import register, Component
+
+
+@register("modal-form")
+class ModalForm(Component):
+    template_name = "modal_form/modal_form.html"
+
+    class Media:
+        js = [
+            "observe-node-insertion.js",
+            "modal_form/modal_form.js",
+        ]
