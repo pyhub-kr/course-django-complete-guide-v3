2ef3bd05ef8b35fc33fe544e627ca5da06872059
Chinseok Lee <me@pyhub.kr>
2024-03-24 10:57:29 +0900

    modal-form 컴포넌트에 커스텀 속성 id 추가

--- mydjango04/components/src-django-components/modal_form/modal_form.py
+++ mydjango04/components/src-django-components/modal_form/modal_form.py
@@ -5,6 +5,9 @@ from django_components.component import register, Component
 class ModalForm(Component):
     template_name = "modal_form/modal_form.html"
 
+    def get_context_data(self, id=None):
+        return {"id": id}
+
     class Media:
         js = [
             "observe-node-insertion.js",
