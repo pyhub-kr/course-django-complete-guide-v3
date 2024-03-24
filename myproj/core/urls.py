4dd1eca67ac870d08aa9b6d73453f1d001f75552
Chinseok Lee <me@pyhub.kr>
2024-03-25 02:01:52 +0900

    최상위 부모 템플릿 및 대문 페이지 구현

--- myproj/core/urls.py
+++ myproj/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 app_name = "core"
 
-urlpatterns = []
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="root.html"), name="root"),
+]
