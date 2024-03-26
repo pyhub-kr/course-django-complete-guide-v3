7e6f349131a897c603f6319cf3ed93ce56a616f8
Chinseok Lee <me@pyhub.kr>
2024-03-27 00:31:15 +0900

    회원가입과 동시에 로그인

--- myproj/accounts/views.py
+++ myproj/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.shortcuts import render
@@ -14,13 +15,17 @@ class SignupView(CreateView):
     form_class = SignupForm
     template_name = "crispy_form.html"
     extra_context = {"form_title": "회원가입"}
-    success_url = reverse_lazy("accounts:login")
+    success_url = reverse_lazy("accounts:profile")
 
     def form_valid(self, form):
         response = super().form_valid(form)
         user: User = self.object
         user.send_welcome_email()
         messages.success(self.request, "회원가입을 환영합니다. ;-)")
+
+        auth_login(self.request, user)
+        messages.success(self.request, "자동 로그인했습니다. :D")
+
         return response
 
 
