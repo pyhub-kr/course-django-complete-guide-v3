c3d25e22d4901bef02fc0a916078bb5d5d2485ff
Chinseok Lee <me@pyhub.kr>
2024-03-26 22:41:28 +0900

    crispy forms 기능으로 로그인 구현

---
+++ myproj/accounts/forms.py
@@ -0,0 +1,13 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginForm(AuthenticationForm):
+    helper = FormHelper()
+    helper.attrs = {"novalidate": True}
+    helper.layout = Layout(
+        "username",
+        "password",
+    )
+    helper.add_input(Submit("submit", "로그인", css_class="w-100"))
--- myproj/accounts/views.py
+++ myproj/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as DjangoLoginView
 
-# Create your views here.
+from accounts.forms import LoginForm
+
+
+class LoginView(DjangoLoginView):
+    form_class = LoginForm
+    template_name = "crispy_form.html"
+    extra_context = {"form_title": "로그인"}
+
+
+login = LoginView.as_view()
