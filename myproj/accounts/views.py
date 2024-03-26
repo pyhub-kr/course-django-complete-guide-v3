265605a86892139f3ae84176e8f390e3bf94a46f
Chinseok Lee <me@pyhub.kr>
2024-03-26 23:00:32 +0900

    프로필 구현

--- myproj/accounts/views.py
+++ myproj/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.shortcuts import render
 
 from accounts.forms import LoginForm
 
@@ -10,3 +12,8 @@ class LoginView(DjangoLoginView):
 
 
 login = LoginView.as_view()
+
+
+@login_required
+def profile(request):
+    return render(request, "accounts/profile.html")
