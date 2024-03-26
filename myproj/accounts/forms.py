7ee85f3d1477789b43ab3f50c5016b15a3125419
Chinseok Lee <me@pyhub.kr>
2024-03-26 23:16:50 +0900

    회원가입 구현

--- myproj/accounts/forms.py
+++ myproj/accounts/forms.py
@@ -1,6 +1,24 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from accounts.models import User
+
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    helper = FormHelper()
+    helper.attrs = {"novalidate": True}
+    helper.layout = Layout(
+        "username",
+        "password1",
+        "password2",
+    )
+    helper.add_input(
+        Submit(name="submit", value="회원가입", css_class="w-100"),
+    )
 
 
 class LoginForm(AuthenticationForm):
--- myproj/accounts/urls.py
+++ myproj/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("profile/", views.profile, name="profile"),
 ]
