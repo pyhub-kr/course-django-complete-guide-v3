39c5451931f2c62fe110188a97b50b1dbee82b5e
Chinseok Lee <me@pyhub.kr>
2024-03-27 00:35:22 +0900

    로그아웃 지원

--- myproj/accounts/urls.py
+++ myproj/accounts/urls.py
@@ -6,5 +6,6 @@ app_name = "accounts"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
     path("profile/", views.profile, name="profile"),
 ]
--- myproj/accounts/views.py
+++ myproj/accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.contrib.auth.views import LogoutView as DjangoLogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -39,6 +40,18 @@ class LoginView(DjangoLoginView):
 
 
 login = LoginView.as_view()
+
+
+class LogoutView(DjangoLogoutView):
+    next_page = "accounts:login"
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.success(request, "로그아웃했습니다.")
+        return response
+
+
+logout = LogoutView.as_view()
 
 
 @login_required
