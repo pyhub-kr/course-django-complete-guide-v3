7cce5c43851f595515de0d1a24b287336b6b7075
Chinseok Lee <me@pyhub.kr>
2024-03-27 13:32:18 +0900

    회원가입 시에 이메일 주소를 필수로 받기

--- myproj/accounts/forms.py
+++ myproj/accounts/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.exceptions import ValidationError
 
 from accounts.models import User
 
@@ -8,10 +9,23 @@ from accounts.models import User
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].required = True
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if email:
+            user_qs = User.objects.filter(email__iexact=email)
+            if user_qs.exists():
+                raise ValidationError("이미 등록된 이메일 주소입니다.")
+        return email
 
     helper = FormHelper()
     helper.attrs = {"novalidate": True}
-    helper.layout = Layout("username", "password1", "password2")
+    helper.layout = Layout("username", "email", "password1", "password2")
     helper.add_input(Submit("submit", "회원가입", css_class="w-100"))
 
 
