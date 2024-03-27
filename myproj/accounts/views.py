f7446665fcaf83179717ba7f56b801f91a0fb0bb
Chinseok Lee <me@pyhub.kr>
2024-03-27 13:05:28 +0900

    회원가입 구현

--- myproj/accounts/views.py
+++ myproj/accounts/views.py
@@ -3,8 +3,29 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.contrib.auth.views import LogoutView as DjangoLogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
+from accounts.models import User
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = "crispy_form.html"
+    extra_context = {
+        "form_title": "회원가입",
+    }
+    success_url = reverse_lazy("accounts:login")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "회원가입을 환영합니다. ;-)")
+        return response
+
+
+signup = SignupView.as_view()
 
 
 class LoginView(DjangoLoginView):
