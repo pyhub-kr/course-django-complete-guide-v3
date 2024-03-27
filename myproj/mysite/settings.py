98f332ac66ed7176e9caa08b59447549bb1ea4d5
Chinseok Lee <me@pyhub.kr>
2024-03-27 09:48:14 +0900

    MEDIA 설정

--- myproj/mysite/settings.py
+++ myproj/mysite/settings.py
@@ -189,6 +189,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "core" / "src-django-components",
 ]
 
+
+# Media files
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- myproj/mysite/urls.py
+++ myproj/mysite/urls.py
@@ -1,4 +1,6 @@
 from django.apps import apps
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,6 +9,10 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("", include("core.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if apps.is_installed("debug_toolbar"):
