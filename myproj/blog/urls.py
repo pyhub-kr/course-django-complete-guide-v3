41397bf75b435f41bed73bb323c5e2afa98f2253
Chinseok Lee <me@pyhub.kr>
2024-03-29 11:29:10 +0900

    blog 앱, 네비게이션 링크 추가

--- myproj/blog/urls.py
+++ myproj/blog/urls.py
@@ -5,4 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("friend_list/", views.friend_list, name="friend_list"),
+    path("new_friend_list/", views.new_friend_list, name="new_friend_list"),
 ]
--- myproj/blog/views.py
+++ myproj/blog/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "blog/index.html")
+
+
+def friend_list(request):
+    return render(request, "blog/friend_list.html")
+
+
+def new_friend_list(request):
+    return render(request, "blog/new_friend_list.html")
