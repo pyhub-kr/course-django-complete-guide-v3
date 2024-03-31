650fbc910bd568997aaaca3bb6a22f238468c5ce
Chinseok Lee <me@pyhub.kr>
2024-03-31 23:22:56 +0900

    carousel 컴포넌트를 활용한 사진 목록 렌더링

---
+++ myproj/core/src-django-components/carousel/carousel.js
@@ -0,0 +1,16 @@
+/*
+ * https://getbootstrap.com/docs/5.3/components/carousel/#how-it-works
+ *
+ * 성능상의 이유로 캐로셀은 Carousel 생성자 메서드를 통해 수동으로 초기화되어야만 합니다.
+ * 그렇지 않으면, 터치/스와이프 지원이 필요한 일부 이벤트 리스너들은 명시적으로 활성화하기 전까지는 등록되지 않습니다.
+ * 예) 두번째 페이지부터 swipe가 동작
+ * For performance reasons, carousels must be manually initialized using the carousel constructor method.
+ * Without initialization, some of the event listeners (specifically, the events needed touch/swipe support) will
+ * not be registered until a user has explicitly activated a control or indicator.
+ */
+
+(function() {
+  observeNodeInsertion(".carousel-component", carouselEl => {
+    const carousel = new bootstrap.Carousel(carouselEl);
+  });
+})();
---
+++ myproj/core/src-django-components/carousel/carousel.py
@@ -0,0 +1,29 @@
+from django_components.component import register, Component
+
+
+@register("carousel")
+class Carousel(Component):
+    template_name = "carousel/carousel.html"
+
+    def get_context_data(self, photo_list, attr_name=None):
+        keys = (attr_name or "").split(".")
+
+        img_url_list = []
+        for attr in photo_list:
+            for key in keys:
+                attr = getattr(attr, key, None)
+            img_url_list.append(attr)
+        return {
+            "img_url_list": img_url_list,
+        }
+
+    class Media:
+        css = {
+            "all": [
+                "carousel/carousel.css",
+            ],
+        }
+        js = [
+            "observe-node-insertion.js",
+            "carousel/carousel.js",
+        ]
