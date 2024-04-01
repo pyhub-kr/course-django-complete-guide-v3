f0cdeded107b223b9431b50956bee8cb8f970736
Chinseok Lee <me@pyhub.kr>
2024-04-01 13:27:29 +0900

    노트 내용의 해시태그 문자열에 A 태그 적용

---
+++ myproj/core/static/hashtag-linkify.js
@@ -0,0 +1,9 @@
+(function () {
+  observeNodeInsertion("[data-hashtag-linkify]", el => {
+    const link = el.getAttribute("data-hashtag-linkify") || "";
+    let html = el.innerHTML;
+    const replacement = `<a href="${link}$1">#$1</a>`;
+    html = html.replace(/#([\wㄱ-힣]+)/g, replacement);
+    el.innerHTML = html;
+  });
+})();
