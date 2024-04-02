86f18b58e0b13a375c9d89f5fc133457fa910788
Chinseok Lee <me@pyhub.kr>
2024-04-02 17:32:22 +0900

    노트 목록에 태그 출력 및 태그 검색 지원

--- myproj/photolog/views.py
+++ myproj/photolog/views.py
@@ -10,7 +10,14 @@ from photolog.models import Note, Photo
 
 
 def index(request):
-    note_qs = Note.objects.all().select_related("author").prefetch_related("photo_set")
+    note_qs = Note.objects.all()
+
+    tag_name = request.GET.get("tag", "").strip()
+    if tag_name:
+        note_qs = note_qs.filter(tags__name__in=[tag_name])
+
+    note_qs = note_qs.select_related("author").prefetch_related("photo_set", "tags")
+
     return render(
         request,
         "photolog/index.html",
