78043d43c8f418a34b8ad76d3d46296a7ebc6e24
Chinseok Lee <me@pyhub.kr>
2024-04-02 12:04:58 +0900

    Photo 이미지가 변경되는 시점에만 이미지 썸네일 처리하기 (Feat. django-lifecycle)

--- myproj/photolog/models.py
+++ myproj/photolog/models.py
@@ -1,9 +1,12 @@
+from io import BytesIO
 from os.path import splitext
 from uuid import uuid4
 
+from PIL import Image
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import force_str
+from django_lifecycle import LifecycleModelMixin, hook, BEFORE_UPDATE
 
 from accounts.models import User
 
@@ -25,7 +28,7 @@ def uuid_name_upload_to(instance: models.Model, filename: str) -> str:
     return "/".join((app_label, cls_name, ymd_path, new_filename))
 
 
-class Photo(models.Model):
+class Photo(LifecycleModelMixin, models.Model):
     note = models.ForeignKey(Note, on_delete=models.CASCADE)
     image = models.ImageField(
         # upload_to="photolog/photo/%Y/%m/%d",
@@ -33,3 +36,19 @@ class Photo(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @hook(BEFORE_UPDATE, when="image", has_changed=True)
+    def on_image_changed(self):
+        if self.image:
+            pil_image = Image.open(self.image.file)
+            MAX_SIZE = (1024, 1024)
+            pil_image.thumbnail(MAX_SIZE)
+            if pil_image.mode == "RGBA":
+                pil_image = pil_image.convert("RGB")
+
+            io = BytesIO()
+            pil_image.save(io, format="jpeg")
+            io.seek(0)
+
+            thumb_name = splitext(self.image.name)[0] + ".jpg"
+            self.image.save(thumb_name, io, save=False)
