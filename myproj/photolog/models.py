555ad172eb45d0b752b4b10f9df6bea9adb0d6e0
Chinseok Lee <me@pyhub.kr>
2024-04-02 11:49:22 +0900

    photolog Note/Photo 모델 정의 및 업로드 파일명을 uuid로 일관되게 저장하기

--- myproj/photolog/models.py
+++ myproj/photolog/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from os.path import splitext
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import force_str
+
+from accounts.models import User
+
+
+class Note(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+def uuid_name_upload_to(instance: models.Model, filename: str) -> str:
+    app_label = instance.__class__._meta.app_label
+    cls_name = instance.__class__.__name__.lower()
+    ymd_path = force_str(timezone.now().strftime("%Y/%m/%d"))
+    extension = splitext(filename)[-1].lower()
+    new_filename = uuid4().hex + extension
+    return "/".join((app_label, cls_name, ymd_path, new_filename))
+
+
+class Photo(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    image = models.ImageField(
+        # upload_to="photolog/photo/%Y/%m/%d",
+        upload_to=uuid_name_upload_to,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
