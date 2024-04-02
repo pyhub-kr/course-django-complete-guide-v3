8c890fd64a465b5cac60c88e4ed9ec3ed1711ffe
Chinseok Lee <me@pyhub.kr>
2024-04-02 11:52:19 +0900

    photolog Note/Photo 모델을 admin에 등록하고 uuid 파일명으로 저장되는 지 확인

--- myproj/photolog/admin.py
+++ myproj/photolog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Note, Photo
 
-# Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
