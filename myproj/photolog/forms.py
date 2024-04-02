f614df3220f28995886e3a403e65d6dbefc0bb4b
Chinseok Lee <me@pyhub.kr>
2024-04-02 16:29:45 +0900

    inlineformset_factory를 활용한 사진 수정 및 삭제

--- myproj/photolog/forms.py
+++ myproj/photolog/forms.py
@@ -4,6 +4,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 from django import forms
 from django.core.files import File
+from django.forms import inlineformset_factory
 
 from .models import Note, Photo
 
@@ -23,21 +24,25 @@ class MultipleImageField(forms.ImageField):
             return single_clean(data)
 
 
-class NoteForm(forms.ModelForm):
-    photos = MultipleImageField()
+class NoteCreateForm(forms.ModelForm):
+    photos = MultipleImageField(required=True)
 
     class Meta:
         model = Note
         fields = ["title", "content"]
 
-    helper = FormHelper()
-    helper.attrs = {"novalidate": True}
-    helper.layout = Layout("title", "content", "photos")
-    helper.add_input(Submit("submit", "저장하기", css_class="w-100"))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.attrs = {"novalidate": True}
+        self.helper.layout = Layout("title", "content", "photos")
+        self.helper.add_input(Submit("submit", "저장하기", css_class="w-100"))
 
     def clean_photos(self):
+        is_required = self.fields["photos"].required
+
         file_list: List[File] = self.cleaned_data.get("photos")
-        if not file_list:
+        if not file_list and is_required:
             raise forms.ValidationError("최소 1개의 사진을 등록해주세요.")
         elif file_list:
             try:
@@ -47,3 +52,30 @@ class NoteForm(forms.ModelForm):
                     "썸네일 생성 중에 오류가 발생했습니다."
                 ) from e
         return file_list
+
+
+class NoteUpdateForm(NoteCreateForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["photos"].required = False
+        self.helper.form_tag = False
+        self.helper.inputs = []
+
+
+class PhotoInlineForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ["image"]
+
+
+# refs: https://django-crispy-forms.readthedocs.io/en/latest/crispy_tag_formsets.html#formsets
+
+PhotoUpdateFormSet = inlineformset_factory(
+    parent_model=Note,
+    model=Photo,
+    form=PhotoInlineForm,
+    extra=0,
+    can_delete=True,
+)
+PhotoUpdateFormSet.helper = FormHelper()
+PhotoUpdateFormSet.helper.form_tag = False
--- myproj/photolog/views.py
+++ myproj/photolog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 
-from photolog.forms import NoteForm
+from photolog.forms import NoteCreateForm, PhotoUpdateFormSet, NoteUpdateForm
 from photolog.models import Note, Photo
 
 
@@ -22,7 +22,7 @@ def index(request):
 
 class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Note
-    form_class = NoteForm
+    form_class = NoteCreateForm
     template_name = "crispy_form.html"
     extra_context = {"form_title": "새 기록"}
 
@@ -48,17 +48,37 @@ note_new = NoteCreateView.as_view()
 @login_required
 def note_edit(request, pk):
     note = get_object_or_404(Note, pk=pk, author=request.user)
+    photo_qs = note.photo_set.all()
 
     if request.method == "GET":
-        form = NoteForm(instance=note)
+        note_form = NoteUpdateForm(instance=note, prefix="note")
+        photo_formset = PhotoUpdateFormSet(
+            queryset=photo_qs,
+            instance=note,
+            prefix="photos",
+        )
     else:
-        form = NoteForm(data=request.POST, files=request.FILES, instance=note)
-        if form.is_valid():
-            saved_note = form.save()
+        note_form = NoteUpdateForm(
+            data=request.POST, files=request.FILES, instance=note, prefix="note"
+        )
+        photo_formset = PhotoUpdateFormSet(
+            queryset=photo_qs,
+            instance=note,
+            data=request.POST,
+            files=request.FILES,
+            prefix="photos",
+        )
 
-            photo_file_list = form.cleaned_data.get("photos")
+        if note_form.is_valid() and photo_formset.is_valid():
+            saved_note = note_form.save()
+
+            # 새롭게 생성되는 Photo
+            photo_file_list = note_form.cleaned_data.get("photos")
             if photo_file_list:
                 Photo.create_photos(saved_note, photo_file_list)
+
+            # 기존 Photo 수정
+            photo_formset.save()
 
             messages.success(request, f"기록#{saved_note.pk}을 수정했습니다.")
 
@@ -66,10 +86,12 @@ def note_edit(request, pk):
 
     return render(
         request,
-        "crispy_form.html",
+        "crispy_form_and_formset.html",
         {
             "form_title": "기록 수정",
-            "form": form,
+            "form": note_form,
+            "formset": photo_formset,
+            "form_submit_label": "저장하기",
         },
     )
 
