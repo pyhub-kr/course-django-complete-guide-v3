17fe2458857c9ba51221f4cf7b001310ec255d68
Chinseok Lee <me@pyhub.kr>
2024-04-03 00:13:27 +0900

    유저 모델에 팔로잉 관계 필드 추가

--- myproj/accounts/models.py
+++ myproj/accounts/models.py
@@ -3,7 +3,24 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following_user_set = models.ManyToManyField(
+        "self",
+        related_name="follower_user_set",
+        symmetrical=False,
+        blank=True,
+    )
+
+    def is_follower(self, to_user: "User") -> bool:
+        return self.following_user_set.filter(id=to_user.id).exists()
+
+    def follow(self, to_user: "User") -> None:
+        self.following_user_set.add(to_user)
+
+    def unfollow(self, to_user: "User") -> None:
+        self.following_user_set.remove(to_user)
+
+    def follower_count(self) -> int:
+        return self.follower_user_set.count()
 
 
 class Profile(models.Model):
