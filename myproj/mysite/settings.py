8b24f2899855b8429b77fbfc7bb944c65e180492
Chinseok Lee <me@pyhub.kr>
2024-04-11 14:32:19 +0900

    ngrok 서비스를 위해, CSRF_TRUSTED_ORIGINS 설정 추가

--- myproj/mysite/settings.py
+++ myproj/mysite/settings.py
@@ -44,6 +44,8 @@ DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
+
 
 # Application definition
 
