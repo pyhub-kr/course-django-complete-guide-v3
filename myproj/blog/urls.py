176ed973fe70944632b1e7295228ada21775dd3a
Chinseok Lee <me@pyhub.kr>
2024-04-24 02:45:44 +0900

    blog:api-v1: 이름공간 추가

--- myproj/blog/urls.py
+++ myproj/blog/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
+from . import api
 
 urlpatterns = []
+
+urlpatterns_api_v1 = []
+
+urlpatterns += [
+    path("api/", include((urlpatterns_api_v1, "api-v1"))),
+]
