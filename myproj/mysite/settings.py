b1306c1d075738733f19ff5c7248976be934bbf1
Chinseok Lee <me@pyhub.kr>
2024-04-25 15:49:40 +0900

    DRF 기본 렌더러에 xlsx/wordcloud renderer 추가

--- myproj/mysite/settings.py
+++ myproj/mysite/settings.py
@@ -264,10 +264,16 @@ COMPONENTS = {
 # djangorestframework
 # https://www.django-rest-framework.org
 
-# REST_FRAMEWORK = {
-#     # Use Django's standard `django.contrib.auth` permissions,
-#     # or allow read-only access for unauthenticated users.
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.AllowAny',
-#     ]
-# }
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.AllowAny',
+    # ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "core.renderers.PandasXlsxRenderer",
+        "core.renderers.WordcloudRenderer",
+    ],
+}
