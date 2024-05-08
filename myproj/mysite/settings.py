df7b5a89e42c22e6319cdd38ff1813c917cee9ed
Chinseok Lee <me@pyhub.kr>
2024-05-08 23:08:06 +0900

    같은 루트 도메인에서 서비스하기

--- myproj/mysite/settings.py
+++ myproj/mysite/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django_components.safer_staticfiles",
     # third apps
-    # "corsheaders",
+    "corsheaders",
     "crispy_forms",
     "crispy_bootstrap5",
     "django_components",
@@ -85,7 +85,7 @@ if DEBUG:
     ]
 
 MIDDLEWARE = [
-    # "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -286,8 +286,10 @@ REST_FRAMEWORK = {
 # django-cors-headers
 # https://github.com/adamchainz/django-cors-headers
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
-#
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://mydj.com:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+SESSION_COOKIE_DOMAIN = ".mydj.com"
