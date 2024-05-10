8a0a3ca578446f6c53dedb45d427d1238ae841b9
Chinseok Lee <me@pyhub.kr>
2024-05-10 15:41:18 +0900

    [mynextjs] WhoamiPage 컴포넌트에서 브라우저 단에서 API 호출하여 CORS 정책 위반

--- mynextjs/pages/blog/index.js
+++ mynextjs/pages/blog/index.js
@@ -1,9 +1,18 @@
 // pages/blog/index.js
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function WhoamiPage() {
   const [message, setMessage] = useState("no message");
+
+  // 컴포넌트 초기화 시에 1회만 실행.
+  useEffect(() => {
+    fetch("http://localhost:8000/blog/whoami/")
+      .then((response) => response.text())
+      .then((responseText) => {
+        setMessage(responseText);
+      });
+  }, []);
 
   return (
     <div>
