44c3c9eabc99c6903535aeab3913a95a5ad31b80
Chinseok Lee <me@pyhub.kr>
2024-05-16 17:50:37 +0900

    이벤트 핸들러 함수를 바르게 지정하기

--- myreact01-cra/src/App4.js
+++ myreact01-cra/src/App4.js
@@ -3,11 +3,13 @@ function Button({ onClick, children }) {
 }
 
 function App() {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
   return (
     <div>
-      <button onClick={() => console.log("clicked element")}>
-        엘리먼트 버튼
-      </button>
+      <button onClick={() => handleClick()}>엘리먼트 버튼</button>
       <Button onClick={() => console.log("clicked component")}>
         컴포넌트 버튼
       </Button>
