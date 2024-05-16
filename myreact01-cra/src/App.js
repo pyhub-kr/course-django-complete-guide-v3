6b8c01ee2b15b8877e0ee9831bf246c3cf2e46fe
Chinseok Lee <me@pyhub.kr>
2024-05-16 15:59:31 +0900

    선언적 UI, Counter 컴포넌트 예시 추가

--- myreact01-cra/src/App.js
+++ myreact01-cra/src/App.js
@@ -1,11 +1,21 @@
+import {useState} from "react";
+
+function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+
+  return (
+    <button onClick={() => setCount(count + 1)}>{count}</button>
+  )
+}
 
 function App() {
   const style = {margin: "1em"};
-  const children = <h1>HELLO WORLD</h1>;
 
   return (
     <div className={"App"} style={style}>
-      {children}
+      <Counter initialCount={10} />
+      <Counter initialCount={20} />
+      <Counter initialCount={30} />
     </div>
   );
 }
