09053e8c9913dfa065b079ed354df9eb0698c686
Chinseok Lee <me@pyhub.kr>
2024-05-16 17:31:53 +0900

    리액트 개발자 도구 동작 실습을 위해, 모든 App 컴포넌트 사용

--- myreact01-cra/src/index.js
+++ myreact01-cra/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-// import App2 from "./App2";
+import App from "./App";
+import App2 from "./App2";
 import App3 from "./App3";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    {/*<App2 />*/}
+    <App />
+    <App2 />
     <App3 />
   </React.StrictMode>,
 );
