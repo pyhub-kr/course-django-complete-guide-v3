1c26722a6e806bbd677c51652d364b9ba032ad9a
Chinseok Lee <me@pyhub.kr>
2024-05-20 23:28:12 +0900

    멜론 검색을 위한 useAxios 수동 요청

--- myreact01-cra/src/App7.jsx
+++ myreact01-cra/src/App7.jsx
@@ -1,63 +1,76 @@
 import useAxios from "axios-hooks";
+import { useState } from "react";
 
+const SEARCH_URL = "https://pyhub.kr/melon/search";
 const DATA_URL = "https://pyhub.kr/melon/20231116.json";
 
-function MelonSongList() {
-  const [{ data: songList = [], loading, error }, refetch] = useAxios(DATA_URL);
+function MelonSongSearch() {
+  const [query, setQuery] = useState("");
+  const [{ data: searchResult = [], loading, error }, search] = useAxios(
+    SEARCH_URL,
+    {
+      manual: true,
+    },
+  );
 
-  // const [songList, setSongList] = useState([]);
-  //
-  // async function loadSingList() {
-  //   const _songList = (await Axios.get(DATA_URL)).data;
-  //   setSongList(_songList);
-  // }
-  //
-  // useEffect(() => {
-  //   loadSingList();
-  // }, []);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      console.log("엔터키가 입력되었습니다. 검색을 수행합니다.");
+      const params = { query };
+      console.log("검색 요청 파라미터 :", params);
+      search({ params });
+    }
+  };
 
   return (
     <div className="container">
-      <h2>멜론 TOP100</h2>
-
       <div className="my-3">
-        <button
-          className="btn btn-primary"
-          onClick={() => refetch()}
-          disabled={loading}
-        >
-          {loading && (
-            <span className="spinner-border spinner-border-sm me-1"></span>
-          )}
-          {!loading && "새로고침"}
-        </button>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="검색어를 입력해주세요."
+          value={query}
+          onChange={(e) => setQuery(e.target.value.trim())}
+          onKeyDown={(e) => handleKeyDown(e)}
+        />
+        {loading && <div className="alert alert-info my-3">로딩 중 ...</div>}
+        {searchResult?.SONGCONTENTS && (
+          <MelonSongList songList={searchResult.SONGCONTENTS} />
+        )}
       </div>
+    </div>
+  );
+}
 
-      {error && <div className="alert alert-danger">에러: {error.message}</div>}
+function MelonSongList({ songList }) {
+  return (
+    <div className="container">
+      <h2>노래 검색결과</h2>
 
       <table className="table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th>앨범</th>
-            <th>곡</th>
-            <th>좋아요</th>
-          </tr>
-        </thead>
         <tbody>
           {songList.map((song) => (
-            <tr key={song.곡일련번호}>
+            <tr key={song.SONGID}>
               <td>
                 <img
-                  src={song.커버이미지_주소}
+                  src={song.ALBUMIMG}
                   className="rounded"
                   style={{ width: "2em" }}
                 />
               </td>
               <td>
-                {song.곡명}
-                <small className="text-sm-start"> - {song.가수}</small>
+                {song.SONGNAME}
+                <small className="text-sm-start"> - {song.ARTISTNAME}</small>
               </td>
-              <td className="text-end">{song.좋아요}</td>
+              <td className="text-end">
+                <a
+                  href={`https://www.melon.com/song/detail.htm?songId=${song.SONGID}`}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  노래 바로가기
+                </a>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -66,4 +79,4 @@ function MelonSongList() {
   );
 }
 
-export default MelonSongList;
+export default MelonSongSearch;
