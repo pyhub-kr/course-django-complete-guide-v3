423c5e810c0933476ad83c337608795afe07d58f
Chinseok Lee <me@pyhub.kr>
2024-05-20 21:46:07 +0900

    할일목록 클릭하여 완료여부 토글

--- myreact01-cra/src/components/TodoList.js
+++ myreact01-cra/src/components/TodoList.js
@@ -10,13 +10,30 @@ const DONE_STYLE = { textDecoration: "line-through" };
 function TodoList() {
   const [todoList, setTodoList] = useState(INITIAL_TODO_LIST);
 
+  const toggleTodo = (todoIndex) => {
+    console.log(`인덱스#${todoIndex}를 토글합니다.`);
+    // todoList[todoIndex].done = !todoList[todoIndex].done;
+
+    const newTodoList = todoList.map((todo, index) => {
+      if (index === todoIndex) {
+        return { ...todo, done: !todo.done };
+      }
+      return todo;
+    });
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
       <h2>할일 목록</h2>
 
-      {todoList.map((todo) => {
+      {todoList.map((todo, index) => {
         return (
-          <li style={{ cursor: "pointer", ...(todo.done ? DONE_STYLE : null) }}>
+          <li
+            key={index}
+            style={{ cursor: "pointer", ...(todo.done ? DONE_STYLE : null) }}
+            onClick={() => toggleTodo(index)}
+          >
             {todo.text}
           </li>
         );
