502cf63c1e5f030ad83cbe9d0c469b8e2cb05869
Chinseok Lee <me@pyhub.kr>
2024-05-20 23:57:21 +0900

    react-router-dom을 통한 페이지 이동

--- myreact01-cra/src/App8.jsx
+++ myreact01-cra/src/App8.jsx
@@ -1,26 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  NavLink,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
 const router = createBrowserRouter([
-  { path: "", element: <div>Home</div> },
-  { path: "blog", element: <div>Blog</div> },
-  { path: "about", element: <div>About</div> },
+  {
+    path: "/",
+    element: (
+      <>
+        <ul>
+          <li>
+            <NavLink to="/">홈</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog">블로그</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">소개</NavLink>
+          </li>
+        </ul>
+        <Outlet />
+      </>
+    ),
+    children: [
+      { index: true, element: <div>Home</div> },
+      { path: "blog", element: <div>Blog</div> },
+      { path: "about", element: <div>About</div> },
+    ],
+  },
 ]);
 
 function App() {
   return (
     <div>
       <h2>라우터</h2>
-      <ul>
-        <li>
-          <a href="/">홈</a>
-        </li>
-        <li>
-          <a href="/blog">블로그</a>
-        </li>
-        <li>
-          <a href="/about">소개</a>
-        </li>
-      </ul>
       <RouterProvider router={router} />
     </div>
   );
