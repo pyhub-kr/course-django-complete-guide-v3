118e8d9078c108c11de1d3b955ea15ddff189bef
Chinseok Lee <me@pyhub.kr>
2024-05-23 14:04:54 +0900

    Axios api 요청에서 401 응답이면, 장고 로그인 페이지로 자동 이동

--- myreact01-cra/src/api.js
+++ myreact01-cra/src/api.js
@@ -1,6 +1,11 @@
 import Axios from "axios";
 import { makeUseAxios } from "axios-hooks";
-import { API_HOST, API_TIMEOUT, API_WITH_CREDENTIALS } from "./constants";
+import {
+  API_HOST,
+  API_TIMEOUT,
+  API_WITH_CREDENTIALS,
+  LOGIN_URL,
+} from "./constants";
 
 const axiosInstance = Axios.create({
   baseURL: API_HOST,
@@ -10,6 +15,17 @@ const axiosInstance = Axios.create({
   //   "Content-Type": "application/json",
   // },
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const next_url = window.location.href;
+      window.location.href = `${LOGIN_URL}?next=${next_url}`;
+    }
+    return Promise.reject(error);
+  },
+);
 
 const useApiAxios = makeUseAxios({
   axios: axiosInstance,
--- myreact01-cra/src/constants.js
+++ myreact01-cra/src/constants.js
@@ -19,4 +19,21 @@ const API_WITH_CREDENTIALS = toBoolean(
   process.env.REACT_APP_API_WITH_CREDENTIALS,
 );
 
-export { API_HOST, API_TIMEOUT, API_WITH_CREDENTIALS };
+const LOGIN_URL =
+  process.env.REACT_APP_LOGIN_URL || `${API_HOST}/accounts/login/`;
+const LOGOUT_URL =
+  process.env.REACT_APP_LOGOUT_URL || `${API_HOST}/accounts/logout/`;
+const SIGNUP_URL =
+  process.env.REACT_APP_LOGIN_URL || `${API_HOST}/accounts/signup/`;
+const PROFILE_URL =
+  process.env.REACT_APP_LOGIN_URL || `${API_HOST}/accounts/profile/`;
+
+export {
+  API_HOST,
+  API_TIMEOUT,
+  API_WITH_CREDENTIALS,
+  LOGIN_URL,
+  LOGOUT_URL,
+  SIGNUP_URL,
+  PROFILE_URL,
+};
