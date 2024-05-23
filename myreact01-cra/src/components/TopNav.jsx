8a7a9ef71932ddc130f7f2ab848e192445cbb756
Chinseok Lee <me@pyhub.kr>
2024-05-23 13:59:35 +0900

    CRA 리액트 페이지에서 세션 인증을 활용한 HTTP 요청
    장고에서 필요한 환경변수
    
    CORS_ALLOWED_ORIGINS=http://localhost:3000
    CORS_ALLOW_CREDENTIALS=true
    
    리액트에서 필요한 환경변수
    
    REACT_APP_API_HOST=http://localhost:8000
    REACT_APP_API_WITH_CREDENTIALS=true

--- myreact01-cra/src/components/TopNav.jsx
+++ myreact01-cra/src/components/TopNav.jsx
@@ -1,42 +1,62 @@
 import { NavLink } from "react-router-dom";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Alert, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useApiAxios } from "../api";
 
 function TopNav() {
+  const [{ data: whoamiHtml = null }] = useApiAxios({
+    url: "/blog/whoami/",
+    headers: {
+      "Content-Type": "text/html",
+    },
+    // withCredentials: true,
+  });
+
+  console.log(whoamiHtml);
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand to={"/"} as={NavLink}>
-          파이썬 사랑방
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto" variant="underline">
-            <Nav.Link to="/blog" as={NavLink}>
-              블로그
-            </Nav.Link>
-            <Nav.Link to="/about" as={NavLink}>
-              소개
-            </Nav.Link>
-            <NavDropdown title="계정" id="basic-nav-dropdown">
-              <NavDropdown.Item to="/accounts/login" as={NavLink}>
-                로그인
-              </NavDropdown.Item>
-              <NavDropdown.Item to="/accounts/signup" as={NavLink}>
-                회원가입
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item to="/accounts/profile" as={NavLink}>
-                프로필
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item to="/accounts/logout" as={NavLink}>
-                로그아웃
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand to={"/"} as={NavLink}>
+            파이썬 사랑방
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto" variant="underline">
+              <Nav.Link to="/blog" as={NavLink}>
+                블로그
+              </Nav.Link>
+              <Nav.Link to="/about" as={NavLink}>
+                소개
+              </Nav.Link>
+              <NavDropdown title="계정" id="basic-nav-dropdown">
+                <NavDropdown.Item to="/accounts/login" as={NavLink}>
+                  로그인
+                </NavDropdown.Item>
+                <NavDropdown.Item to="/accounts/signup" as={NavLink}>
+                  회원가입
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item to="/accounts/profile" as={NavLink}>
+                  프로필
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item to="/accounts/logout" as={NavLink}>
+                  로그아웃
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {whoamiHtml && (
+        <Container>
+          <Alert variant="info" className="mt-2">
+            <div dangerouslySetInnerHTML={{ __html: whoamiHtml }} />
+          </Alert>
+        </Container>
+      )}
+    </>
   );
 }
 
