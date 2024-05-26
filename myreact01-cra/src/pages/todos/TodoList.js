9e08d53838772c5a20c1f77e4740884931c204a7
Chinseok Lee <me@pyhub.kr>
2024-05-27 01:04:19 +0900

    Todo 목록 API 연동

--- myreact01-cra/src/pages/todos/TodoList.js
+++ myreact01-cra/src/pages/todos/TodoList.js
@@ -1,35 +1,23 @@
 import React, { useState } from "react";
 import { produce } from "immer";
 import TodoForm from "../../components/TodoForm";
-import { Button, Card, Container, ListGroup } from "react-bootstrap";
-
-const INITIAL_TODO_LIST = [
-  { text: "파이썬 익히기", done: true },
-  { text: "장고 익히기", done: false },
-];
+import { Button, Card, Container, ListGroup, Spinner } from "react-bootstrap";
+import { useApiAxios } from "../../api";
 
 const DONE_STYLE = { textDecoration: "line-through" };
 
 function TodoList() {
-  const [todoList, setTodoList] = useState(INITIAL_TODO_LIST);
+  const [{ data: todoList = [], loading, error: loadingError }] =
+    useApiAxios("/blog/api/todos/");
 
   const toggleTodo = (todoIndex) => {
     console.log(`인덱스#${todoIndex}를 토글합니다.`);
-    // todoList[todoIndex].done = !todoList[todoIndex].done;
 
-    // const newTodoList = todoList.map((todo, index) => {
-    //   if (index === todoIndex) {
-    //     return { ...todo, done: !todo.done };
-    //   }
-    //   return todo;
-    // });
-    // setTodoList(newTodoList);
-
-    setTodoList(
-      produce((draftTodoList) => {
-        draftTodoList[todoIndex].done = !draftTodoList[todoIndex].done;
-      }),
-    );
+    // setTodoList(
+    //   produce((draftTodoList) => {
+    //     draftTodoList[todoIndex].done = !draftTodoList[todoIndex].done;
+    //   }),
+    // );
   };
 
   // const handleSubmit = (e) => {
@@ -41,14 +29,14 @@ function TodoList() {
   // };
 
   const addTodo = (newText) => {
-    setTodoList((prev) => [...prev, { text: newText, done: false }]);
+    // setTodoList((prev) => [...prev, { text: newText, done: false }]);
   };
 
   const deleteTodo = (todoIndex) => {
     if (window.confirm("정말 삭제하시겠습니까?")) {
-      setTodoList((prev) => {
-        return prev.filter((_, index) => index !== todoIndex);
-      });
+      // setTodoList((prev) => {
+      //   return prev.filter((_, index) => index !== todoIndex);
+      // });
     }
   };
 
@@ -57,20 +45,36 @@ function TodoList() {
     const origText = todo.text;
     const promptText = window.prompt("수정할 내용을 입력하세요.", origText);
     if (promptText !== null && promptText !== origText) {
-      setTodoList(
-        produce((draftTodoList) => {
-          draftTodoList[todoIndex].text = promptText;
-        }),
-      );
+      // setTodoList(
+      //   produce((draftTodoList) => {
+      //     draftTodoList[todoIndex].text = promptText;
+      //   }),
+      // );
     }
   };
 
   return (
     <Container>
       <Card>
-        <Card.Header>할일 목록</Card.Header>
+        <Card.Header>
+          할일 목록
+          {loading && (
+            <Spinner
+              animation="grow"
+              variant="primary"
+              size="sm"
+              className="mx-1"
+            />
+          )}
+        </Card.Header>
 
         <ListGroup variant="flush">
+          {loadingError && (
+            <ListGroup.Item variant="danger">
+              {loadingError.message}
+            </ListGroup.Item>
+          )}
+
           {todoList.length === 0 && (
             <ListGroup.Item variant="warning">
               등록된 할일이 없습니다.
