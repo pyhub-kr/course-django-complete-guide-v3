d15400a035642eda06980eacf73afd796c54e7f2
Chinseok Lee <me@pyhub.kr>
2024-05-27 00:53:38 +0900

    axiosInstance를 통한 API 요청에서의 응답 에러에 대해서 일관되게 Toast 메시지 처리

--- myreact01-cra/src/api.js
+++ myreact01-cra/src/api.js
@@ -1,5 +1,6 @@
 import Axios from "axios";
 import { makeUseAxios } from "axios-hooks";
+import { toast } from "react-toastify";
 import {
   API_HOST,
   API_TIMEOUT,
@@ -33,7 +34,38 @@ axiosInstance.interceptors.response.use(
     if (error.response?.status === 401) {
       const next_url = window.location.href;
       window.location.href = `${LOGIN_URL}?next=${next_url}`;
+    } else if (error.response?.status === 400) {
+      // 유효성 검사에 실패했을 때
+      const errorMessages = error.response.data;
+      toast.error(
+        <>
+          <ul className="list-unstyled">
+            {Object.entries(errorMessages).map(([fieldName, fieldErrors]) => (
+              <li key={fieldName}>
+                <strong>{fieldName}</strong>: {fieldErrors.join(" ")}
+              </li>
+            ))}
+          </ul>
+        </>,
+      );
+    } else if (error.code === "ERR_CANCELED") {
+      // 요청이 취소되었을 때
+      /* skip */
+    } else if (error.code === "ERR_NETWORK") {
+      // 네트워크 오류
+      console.error("에러 발생:", error);
+      toast.error(`${error?.config?.url} 요청 중 네트워크 에러 발생`);
+    } else if (error.response) {
+      // 서버 응답이 있는 400, 401 외의 에러 상황
+      // DRF 에러 응답 포맷에 따라 에러 메시지를 보여줍니다.
+      const errorMessage = error.response.data?.detail || error.message;
+      toast.error(`[${error.response.status}] ${errorMessage}`);
+    } else {
+      // 그 외의 에러 상황
+      console.log("에러 발생:", error);
+      toast.error(error.message);
     }
+
     return Promise.reject(error);
   },
 );
