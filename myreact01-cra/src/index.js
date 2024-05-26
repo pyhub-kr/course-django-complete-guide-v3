387985a04927d11dc7c79b4df382193fd1e81b62
Chinseok Lee <me@pyhub.kr>
2024-05-27 00:49:58 +0900

    react-toastify 라이브러리 설치

--- myreact01-cra/src/index.js
+++ myreact01-cra/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 import "./index.css";
 import App from "./App";
 import App2 from "./App2";
@@ -13,17 +14,30 @@ import App8 from "./App8";
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 
+import "react-toastify/dist/ReactToastify.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    {/*<App2 />*/}
-    {/*<App3 />*/}
-    {/*<App4 />*/}
-    {/*<App5 />*/}
-    {/*<App6 />*/}
-    {/*<App7 />*/}
-    <App8 />
+    <>
+      {/*<App />*/}
+      {/*<App2 />*/}
+      {/*<App3 />*/}
+      {/*<App4 />*/}
+      {/*<App5 />*/}
+      {/*<App6 />*/}
+      {/*<App7 />*/}
+      <App8 />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        newestOnTop={true}
+        closeOnClick={true}
+        hideProgressBar={true}
+        pauseOnHover={true}
+        draggable={false}
+      />
+    </>
   </React.StrictMode>,
 );
 
