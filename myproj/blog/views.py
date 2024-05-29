e6afe396391cb909d4c75b9ed62e78fa4122ae57
Chinseok Lee <me@pyhub.kr>
2024-05-29 11:38:37 +0900

    blog:index 뷰에서 포스팅 목록 HTML을 반환토록 하고, cache_page 장식자 적용

--- myproj/blog/views.py
+++ myproj/blog/views.py
@@ -1,26 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from django.views.generic import CreateView, UpdateView
 from django_nextjs.render import render_nextjs_page
 
 from blog.forms import TodoForm
-from blog.models import Todo
+from blog.models import Todo, Post
 from blog.serializers import TodoSerializer
+
+
+@cache_page(600)
+def index(request):
+    post_qs = Post.objects.all()
+    return render(
+        request,
+        "blog/index.html",
+        {
+            "post_list": post_qs,
+        },
+    )
 
 
 def whoami(request):
     status = 200 if request.user.is_authenticated else 401
     username = request.user.username or "anonymous"
     return HttpResponse(f"Your username is <strong>{username}</strong>.", status=status)
-
-
-async def index(request):
-    # return render(request, "blog/index.html")
-    return await render_nextjs_page(
-        request,
-        template_name="blog/index.html",
-        context={},
-    )
 
 
 class TodoCreateView(CreateView):
